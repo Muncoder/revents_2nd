92fbbd1496613efd8fe192008278ef5f567b3ce2
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-11-29 22:46:38 +0000

    21. building our first component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import EventDashboard from '../../features/EventDashboard/EventDashboard'
 
 class App extends Component {
   render() {
     return (
       <div>
 				<h1>Hello World</h1>
+				<EventDashboard />
       </div>
     );
   }
