ff1e369e25ea0ef79ff9b52aef87045ee3a2f42f
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-12-02 20:44:34 +0000

    48. using the withRouter higher order component

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Menu, Container, Button } from 'semantic-ui-react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link, withRouter } from 'react-router-dom'
 import SignedOutMenu from '../Menus/SignedOutMenu'
 import SignedInMenu from '../Menus/SignedInMenu'
 
@@ -32,10 +32,14 @@ class NavBar extends Component {
             Re-vents
           </Menu.Item>
 					<Menu.Item as={NavLink} to='/events' name="Events" />
-					<Menu.Item as={NavLink} to='/people' name="People" />
+					{ authenticated && 
+					<Menu.Item as={NavLink} to='/people' name="People" /> }
+
+					{ authenticated && 
           <Menu.Item>
 						<Button as={Link} to='/createEvent' floated="right" positive inverted content="Create Event" />
-          </Menu.Item>
+					</Menu.Item>}
+
 					{ authenticated ? (
 						<SignedInMenu signOut={this.handleSignOut} />
 						)	: (
@@ -47,4 +51,4 @@ class NavBar extends Component {
 	}
 }
 
-export default NavBar;
+export default withRouter(NavBar);
