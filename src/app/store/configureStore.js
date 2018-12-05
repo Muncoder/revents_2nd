02556315410a75fee994b2143ae49da2365ebc81
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-12-05 23:03:21 +0000

    55. connecting the store to our application

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers/rootReducer'
 
 export const configureStore = (preloadedState) => {
-	const middleWares = []
+	const middlewares = []
 	const middlewareEnhancer = applyMiddleware(...middlewares);
 
 	const storeEnhancers = [middlewareEnhancer];
