32e2eb7cfbd426b14456e58d235426065f3ab30b
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-12-05 20:19:22 +0000

    53. redux store configuration

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+
+export const configureStore = (preloadedState) => {
+	const middleWares = []
+	const middlewareEnhancer = applyMiddleware(...middlewares);
+
+	const storeEnhancers = [middlewareEnhancer];
+
+	const composedEnhancer = compose(...storeEnhancers);
+
+	const store = createStore(
+		rootReducer,
+		preloadedState,
+		composedEnhancer
+	)
+
+	return store
+}
