cb4600e8d68fcbfb34cbd81a6aa4350d721e0812
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-12-08 18:00:46 +0000

    75. redux forms setup

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { reduxForm, Field } from 'redux-form'
 import cuid from 'cuid'
 import { Segment, Form, Button } from 'semantic-ui-react'
 import { createEvent, updateEvent } from '../eventActions'
@@ -64,10 +65,7 @@ class EventForm extends Component {
 		return (
 			<Segment>
 				<Form onSubmit={this.onFormSubmit}>
-					<Form.Field>
-						<label>Event Title</label>
-						<input name='title' onChange={this.onInputChange} value={event.title} placeholder="Event Title" />
-					</Form.Field>
+					<Field name='title' type='text' component='input' placeholder='Event Title' />
 					<Form.Field>
 						<label>Event Date</label>
 						<input name='date' onChange={this.onInputChange} value={event.date} type="date" placeholder="Event Date" />
@@ -94,4 +92,4 @@ class EventForm extends Component {
 	}
 }
 
-export default connect(mapState, actions)(EventForm);
+export default connect(mapState, actions)(reduxForm({form: 'eventForm'})(EventForm));
