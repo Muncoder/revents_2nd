410af056bd068b9a3ada28aac0b8f0de95eb7f96
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-12-08 18:35:23 +0000

    76. creating a reusable text input field

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -4,6 +4,7 @@ import { reduxForm, Field } from 'redux-form'
 import cuid from 'cuid'
 import { Segment, Form, Button } from 'semantic-ui-react'
 import { createEvent, updateEvent } from '../eventActions'
+import TextInput from '../../../app/common/form/TextInput'
 
 const mapState = (state, ownProps) => {
 	const eventId = ownProps.match.params.id;
@@ -65,7 +66,7 @@ class EventForm extends Component {
 		return (
 			<Segment>
 				<Form onSubmit={this.onFormSubmit}>
-					<Field name='title' type='text' component='input' placeholder='Event Title' />
+					<Field name='title' type='text' component={TextInput} placeholder='Give your events a name' />
 					<Form.Field>
 						<label>Event Date</label>
 						<input name='date' onChange={this.onInputChange} value={event.date} type="date" placeholder="Event Date" />
