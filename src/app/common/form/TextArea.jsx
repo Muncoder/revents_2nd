ff9cb0b103617260bef707a9c8c3d1922e4a0784
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-12-08 19:32:17 +0000

    79. creating a reusable Select Input field

--- src/app/common/form/TextArea.jsx
+++ src/app/common/form/TextArea.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Form, Label } from 'semantic-ui-react'
 
 const TextArea = ({ input, rows, type, placeholder, meta: {touched, error}}) => {
+	console.log(input)
 	return (
 		<Form.Field error={touched && !!error}>
 			<textarea {...input} placeholder={placeholder} rows={rows}></textarea>
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -6,6 +6,7 @@ import { Segment, Form, Button, Grid, Header } from 'semantic-ui-react'
 import { createEvent, updateEvent } from '../eventActions'
 import TextInput from '../../../app/common/form/TextInput'
 import TextArea from '../../../app/common/form/TextArea'
+import SelectInput from '../../../app/common/form/SelectInput'
 
 const mapState = (state, ownProps) => {
 	const eventId = ownProps.match.params.id;
@@ -31,6 +32,15 @@ const actions = {
 	createEvent,
 	updateEvent
 }
+
+const category = [
+	{key: 'drinks', text: 'Drinks', value: 'drinks'},
+	{key: 'culture', text: 'Culture', value: 'culture'},
+	{key: 'film', text: 'Film', value: 'film'},
+	{key: 'food', text: 'Food', value: 'food'},
+	{key: 'music', text: 'Music', value: 'music'},
+	{key: 'travel', text: 'Travel', value: 'travel'}
+]
 
 class EventForm extends Component {
 
@@ -58,7 +68,7 @@ class EventForm extends Component {
 						<Header sub color='teal' content='Event Details' />
 						<Form onSubmit={this.onFormSubmit}>
 							<Field name='title' type='text' component={TextInput} placeholder='Give your events a name' />
-							<Field name='category' type='text' component={TextInput} placeholder='What is your event about' />
+							<Field name='category' type='text' component={SelectInput} options={category} placeholder='What is your event about' />
 							<Field name='description' type='text' rows={3} component={TextArea} placeholder='Tell us about your event' />
 
 							<Header sub color='teal' content='Event Location Details' />
