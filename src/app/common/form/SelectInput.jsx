9cbf1d357cd30b546cbb893b041ac205a25956c8
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-12-08 20:28:19 +0000

    80. form submission is redux form

--- src/app/common/form/SelectInput.jsx
+++ src/app/common/form/SelectInput.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Form, Label, Select } from 'semantic-ui-react'
 
 const SelectInput = ({ input, type, placeholder, multiple, options,  meta: {touched, error}}) => {
-	console.log(input)
 	return (
 		<Form.Field error={touched && !!error}>
 			<Select
